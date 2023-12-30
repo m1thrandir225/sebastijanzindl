c86ff4e7b908109823628676e81a7c6f710236cb
Sebastijan Zindl <sebastijan32@hotmail.com>
2023-12-30 18:52:13 +0100

    style: style changes for mobile

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -4,7 +4,7 @@
         class="gap-4 flex flex-col justify-start w-full h-full p-8 mx-auto transition-colors duration-200 ease-in-out rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-200 dark:bg-neutral-800 overflow-y-auto md:max-h-[90%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-thumb-neutral-900 scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar dark:scrollbar-track-neutral-700/20"
     >
         <div
-            class="flex flex-col items-center justify-between w-full md:flex-row"
+            class="flex flex-col items-start justify-between w-full md:items-center md:flex-row"
         >
             <h1
                 class="font-sans text-xl font-bold text-neutral-900 dark:text-neutral-100"
@@ -13,7 +13,7 @@
             </h1>
             <p
                 v-if="post?.publishedAt"
-                class="text-lg text-neutral-900 dark:text-neutral-100"
+                class="text-md md:text-lg text-neutral-900 dark:text-neutral-100"
             >
                 Published on:
                 {{ new Date(post?.publishedAt).toLocaleString('en-US') }}
