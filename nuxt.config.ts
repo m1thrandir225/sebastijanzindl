c4323ef710baf0dbf6a334aec04dbfef74bcdd4f
Sebastijan Zindl <sebastijan32@hotmail.com>
2024-01-17 23:05:23 +0100

    fix: background color added to bod tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
         head: {
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
+            bodyAttrs: {
+                class: 'bg-neutral-100 dark:bg-neutral-900',
+            },
         },
     },
     modules: [
