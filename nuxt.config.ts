9c531bef1a92c00dab2ce4f4c6943073062f3393
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-02-01 22:47:16 +0100

    feat: added a middleware for generating a robots.txt file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
         '@nuxt/image',
         '@vueuse/motion/nuxt',
         '@nuxtjs/sanity',
+        '@nuxtjs/robots',
     ],
     colorMode: {
         classSuffix: '',
