ad5f50fd83f41d2c9aa1a584b723d1451eaf5ccd
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-07-15 16:59:04 +0200

    feat: incremental static regeneration added to all pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,56 +1,56 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    app: {
-        head: {
-            charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
-            bodyAttrs: {
-                class: 'bg-neutral-100 dark:bg-neutral-900',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      bodyAttrs: {
+        class: 'bg-neutral-100 dark:bg-neutral-900',
+      },
+    },
+  },
+  routeRules: {
+    '/': { prerender: true, isr: 3600 },
+    '/blog': { prerender: true, isr: 3600 },
+    '/posts/**': { prerender: true, isr: 3600 },
+    '/projects': { prerender: true, isr: 3600 },
+  },
+  modules: [
+    'floating-vue/nuxt',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/image',
+    '@vueuse/motion/nuxt',
+    '@nuxtjs/sanity',
+    '@nuxtjs/robots',
+  ],
+  colorMode: {
+    classSuffix: '',
+    preference: 'dark',
+  },
+  sanity: {
+    projectId: 'ptgttce5',
+  },
+  runtimeConfig: {
+    public: {
+      motion: {
+        directives: {
+          'pop-bottom': {
+            initial: {
+              scale: 0,
+              opacity: 0,
+              y: 100,
             },
-        },
-    },
-    routeRules: {
-        '/': { prerender: true, ssr: true },
-        '/blog': { prerender: true },
-        '/posts/**': { isr: true, prerender: true },
-        '/projects': { isr: true },
-    },
-    modules: [
-        'floating-vue/nuxt',
-        '@nuxtjs/eslint-module',
-        '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode',
-        '@nuxt/image',
-        '@vueuse/motion/nuxt',
-        '@nuxtjs/sanity',
-        '@nuxtjs/robots',
-    ],
-    colorMode: {
-        classSuffix: '',
-        preference: 'dark',
-    },
-    sanity: {
-        projectId: 'ptgttce5',
-    },
-    runtimeConfig: {
-        public: {
-            motion: {
-                directives: {
-                    'pop-bottom': {
-                        initial: {
-                            scale: 0,
-                            opacity: 0,
-                            y: 100,
-                        },
-                        visible: {
-                            scale: 1,
-                            opacity: 1,
-                            y: 0,
-                        },
-                    },
-                },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              y: 0,
             },
+          },
         },
+      },
     },
+  },
 })
