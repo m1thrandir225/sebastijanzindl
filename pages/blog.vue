d61e67d07581300f232ef1e9dd2b13b875f94bc4
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-08-16 18:19:47 +0200

    style: some style changes and some subheadings changed

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,27 +1,16 @@
 <template>
-    <div
-        class="flex flex-col items-start justify-start w-full h-full max-w-screen-xl gap-2 mx-auto"
-    >
+    <div class="flex flex-col items-start justify-start w-full h-full max-w-screen-xl gap-2 mx-auto">
         <div class="flex flex-col items-start justify-start gap-2 p-4 md:p-0">
-            <h1
-                class="font-sans text-3xl font-bold dark:text-neutral-100 text-neutral-800"
-            >
+            <h1 class="font-sans text-3xl font-bold dark:text-neutral-100 text-neutral-800">
                 Blog
             </h1>
-            <p
-                class="font-sans text-lg font-medium dark:text-neutral-100 text-neutral-800"
-            >
-                Maybe I'll write something here, maybe not.
+            <p class="font-sans text-lg font-medium dark:text-neutral-100 text-neutral-800">
+                Hopefully this will be a thriving blog some day.
             </p>
         </div>
         <div
-            class="gap-4 flex flex-col justify-start w-full h-full p-8 mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-200 dark:bg-neutral-800 overflow-y-auto md:max-h-[90%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-thumb-neutral-900 scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar dark:scrollbar-track-neutral-700/20"
-        >
-            <PostOverview
-                v-for="post in posts"
-                :key="post.slug.current"
-                :post="post"
-            />
+            class="gap-4 flex flex-col justify-start w-full h-full p-8 mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-200 dark:bg-neutral-800 overflow-y-auto md:max-h-[90%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-thumb-neutral-900 scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar dark:scrollbar-track-neutral-700/20">
+            <PostOverview v-for="post in posts" :key="post.slug.current" :post="post" />
         </div>
     </div>
 </template>
@@ -41,9 +30,9 @@ useHead({
 
 useSeoMeta({
     title: 'Sebastijan Zindl - Blog',
-    description: "Maybe I'll write something here, maybe not.",
+    description: "My Personal Blog",
     ogTitle: 'Sebastijan Zindl - Blog',
-    ogDescription: "Maybe I'll write something here, maybe not.",
+    ogDescription: "My Personal Blog",
     ogType: 'website',
     ogUrl: 'https://sebastijanzindl.me/blog',
 })
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,71 +1,32 @@
 <template>
-    <div
-        class="relative flex flex-col items-start w-full h-full max-w-screen-xl gap-4 mx-auto justift-start"
-    >
-        <div
-            class="relative flex flex-col items-start w-full gap-2 p-4 text-start justift-start md:p-0"
-        >
-            <h1
-                class="font-sans text-3xl font-bold dark:text-neutral-100 text-neutral-800"
-            >
+    <div class="relative flex flex-col items-start w-full h-full max-w-screen-xl gap-4 mx-auto justift-start">
+        <div class="relative flex flex-col items-start w-full gap-2 p-4 text-start justift-start md:p-0">
+            <h1 class="font-sans text-3xl font-bold dark:text-neutral-100 text-neutral-800">
                 Projects
             </h1>
-            <p
-                class="font-sans text-lg font-medium dark:text-neutral-100 text-neutral-800"
-            >
-                Some recent projects I've made.
+            <p class="font-sans text-lg font-medium dark:text-neutral-100 text-neutral-800">
+                Here is some of my work.
             </p>
         </div>
         <div
-            class="relative z-50 flex flex-col justify-center w-auto h-full p-8 mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-200 dark:bg-neutral-800"
-        >
+            class="relative z-50 flex flex-col max-h-[600px] justify-center w-auto h-full p-8 mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-200 dark:bg-neutral-800">
             <div v-if="status === 'pending'" class="self-center">
-                <LucideIcon
-                    name="Loader2"
-                    class="animate-spin text-neutral-900 dark:text-neutral-200"
-                    :size="64"
-                />
+                <LucideIcon name="Loader2" class="animate-spin text-neutral-900 dark:text-neutral-200" :size="64" />
             </div>
-            <div
-                v-else-if="status === 'error'"
-                class="flex flex-row items-center self-center justify-center gap-4"
-            >
-                <LucideIcon
-                    name="HeartCrack"
-                    class="text-neutral-900 dark:text-neutral-200"
-                    :size="64"
-                />
-                <p
-                    class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200"
-                >
+            <div v-else-if="status === 'error'" class="flex flex-row items-center self-center justify-center gap-4">
+                <LucideIcon name="HeartCrack" class="text-neutral-900 dark:text-neutral-200" :size="64" />
+                <p class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200">
                     Something went wrong...
                 </p>
             </div>
 
-            <div
-                v-else-if="
-                    status === 'success' && projects && projects?.length > 0
-                "
-                class="relative z-50 grid py-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start justify-start h-full gap-4 overflow-y-auto md:max-h-[95%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar scrollbar-thumb-neutral-100 dark:scrollbar-track-neutral-700/20"
-            >
-                <ProjectOverview
-                    v-for="project in projects"
-                    :key="project._id"
-                    :project="project"
-                />
+            <div v-else-if="status === 'success' && projects && projects.length > 0"
+                class="relative z-50 grid py-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-start justify-start h-full gap-4 overflow-y-auto md:max-h-[95%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar scrollbar-thumb-neutral-100 dark:scrollbar-track-neutral-700/20">
+                <ProjectOverview v-for="project in projects" :key="project._id" :project="project" />
             </div>
-            <div
-                v-else-if="status === 'success' && !projects"
-                class="flex items-center self-center justify-center gap-4"
-            >
-                <LucideIcon
-                    name="Meh"
-                    class="text-neutral-900 dark:text-neutral-100"
-                    :size="64"
-                />
-                <p
-                    class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200"
-                >
+            <div v-else-if="status === 'success' && !projects" class="flex items-center self-center justify-center gap-4">
+                <LucideIcon name="Meh" class="text-neutral-900 dark:text-neutral-100" :size="64" />
+                <p class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200">
                     No projects found.
                 </p>
             </div>
