eee079454e0bf2eefb4878a529887089e97e17f6
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2025-12-30 13:10:00 +0100

    fix: projects appear animation & mobile single-project title fix

--- components/project-card-v3.vue
+++ components/project-card-v3.vue
@@ -1,47 +1,51 @@
+<script setup lang="ts">
+import LucideIcon from '@/components/lucide-icon.vue';
+import type { Project } from '~/types/content/project';
+
+const props = defineProps<{
+    project: Project
+    index: number
+}>()
+
+const { isMobile } = useDevice()
+</script>
+
 <template>
     <NuxtLink
-        v-motion
-        :to="{
-            name: 'projects-slug',
-            params: { slug: props.project.slug.current },
-        }"
-        :initial="{
+    :key="props.project._id" 
+    v-motion
+    :to="{
+        name: 'projects-slug',
+        params: { slug: props.project.slug.current },
+    }" :initial="{
             opacity: 0,
-            scale: 0,
-        }"
-        :visible-once="{
-            opacity: 100,
+            scale: 0.95,
+        }" :visible-once="{
+            opacity: 1,
             scale: 1,
             transition: {
-                delay: index < 7 ? 400 + 75 * index : 250 + 15 * index,
+                delay: (() => {
+                    if(isMobile) {
+                         return index < 4 ? 100 + 75 * index : 0
+                    } else {
+                        return index < 6 ? 100 + 75 * index : 0
+                    }
+                })(),
+                duration: 300,
             },
         }"
-        class="w-full flex flex-row items-center justify-between p-6 dark:outline-neutral-700 rounded-xl group hover:bg-neutral-200 dark:hover:bg-neutral-700 transition-all ease-in-out duration-300 cursor-pointer"
-    >
+        class="w-full flex flex-row items-center justify-between p-6 dark:outline-neutral-700 rounded-xl group hover:bg-neutral-200 dark:hover:bg-neutral-700 transition-all ease-in-out duration-300 cursor-pointer">
         <div class="flex flex-col gap-2">
             <h1
-                class="font-array text-left font-bold text-neutral-900 dark:text-neutral-100 group-hover:text-brand transition-all ease-in-out duration-300 group-hover:dark:text-brand group-hover:underline group-hover:decoration-wavy group-hover:decoration-pink-400"
-            >
+                class="font-array text-left font-bold text-neutral-900 dark:text-neutral-100 group-hover:text-brand transition-all ease-in-out duration-300 group-hover:dark:text-brand group-hover:underline group-hover:decoration-wavy group-hover:decoration-pink-400">
                 {{ project.title }}
             </h1>
             <p class="font-medium italic text-[14px] opacity-85">
                 {{ project.shortDescription }}
             </p>
         </div>
-        <LucideIcon
-            name="ArrowRight"
+        <LucideIcon name="ArrowRight"
             class="rotate-180 group-hover:rotate-0 transition-all ease-in-out duration-300 group-hover:text-brand group-hover:scale-125"
-            :size="24"
-        />
+            :size="24" />
     </NuxtLink>
 </template>
-
-<script setup lang="ts">
-import LucideIcon from '@/components/lucide-icon.vue'
-import type { Project } from '~/types/content/project'
-
-const props = defineProps<{
-    project: Project
-    index: number
-}>()
-</script>
--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router'
 import LucideIcon from '@/components/lucide-icon.vue'
 import type { Project } from '~/types/content/project'
 import TechnologyItem from '~/components/technology-item.vue'
+import { useMediaQuery } from '@vueuse/core'
 
 const route = useRoute()
 
@@ -23,6 +24,8 @@ const {
 })
 
 const router = useRouter()
+
+const isSmallScreen = useMediaQuery('(max-width: 640px)')
 
 function goBack() {
     router.back()
@@ -50,7 +53,83 @@ useSeoMeta({
         v-if="status !== 'pending' && project"
         class="relative flex flex-col items-start w-full h-full px-4 md:px-0 gap-4 mx-auto justift-start"
     >
-        <div class="w-full flex flex-row items-center justify-between">
+        <div
+            v-if="isSmallScreen"
+            class="w-full flex flex-col gap-4 items-start"
+        >
+            <div class="w-full flex flex-row items-center justify-between">
+                <button
+                    class="group p-2 rounded-md bg-neutral-800 dark:bg-neutral-100"
+                    @click="goBack"
+                >
+                    <LucideIcon
+                        name="ArrowLeft"
+                        class="transition-all text-neutral-100 dark:text-neutral-800 ease-in-out duration-300 group-hover:text-brand group-hover:scale-125"
+                        :size="18"
+                    />
+                </button>
+                <div class="flex flex-row items-center gap-4">
+                    <NuxtLink
+                        v-if="project.githubLink"
+                        v-motion
+                        :initial="{
+                            opacity: 0,
+                            x: -5,
+                        }"
+                        :enter="{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                delay: 200,
+                            },
+                        }"
+                        :href="project.githubLink"
+                        target="_blank"
+                        class="text-neutral-900 dark:text-neutral-100 hover:text-brand transition-all ease-in-out duration-300"
+                    >
+                        <LucideIcon name="Github" :size="24" />
+                    </NuxtLink>
+                    <NuxtLink
+                        v-if="project.productionLink"
+                        v-motion
+                        :href="project.productionLink"
+                        :initial="{
+                            opacity: 0,
+                            x: 5,
+                        }"
+                        :enter="{
+                            opacity: 1,
+                            x: 0,
+                            transition: {
+                                delay: 250,
+                            },
+                        }"
+                        target="_blank"
+                        class="text-neutral-900 dark:text-neutral-100 hover:text-brand transition-all ease-in-out duration-300"
+                    >
+                        <LucideIcon name="Link" :size="24" />
+                    </NuxtLink>
+                </div>
+            </div>
+            <h1
+                v-motion
+                :initial="{
+                    opacity: 0,
+                    y: -50,
+                }"
+                :enter="{
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                        delay: 0,
+                    },
+                }"
+                class="font-array font-bold text-3xl dark:text-neutral-100 text-neutral-800"
+            >
+                {{ project.title }}
+            </h1>
+        </div>
+        <div v-else class="w-full flex flex-row items-center justify-between">
             <div class="flex flex-row gap-4 items-center">
                 <button
                     class="group p-2 rounded-md bg-neutral-800 dark:bg-neutral-100"
@@ -142,7 +221,7 @@ useSeoMeta({
             :image="project.image"
             :asset-id="project.image.asset._ref"
             auto="format"
-            class="rounded-xl transition-all ease-in duration-200 border-2 border-transparent hover:border-brand md:h-[380px] w-full object-cover min-h-[200px]"
+            class="rounded-xl transition-all ease-in duration-200 border-2 border-transparent hover:border-brand"
         />
         <div
             v-motion
@@ -160,7 +239,7 @@ useSeoMeta({
             }"
         >
             <p
-                class="font-array text-[18px] lg:text-[24px] italic md:text-lg font-medium dark:text-neutral-100 text-neutral-800"
+                class="font-array text-[20px] lg:text-[24px] italic font-medium dark:text-neutral-100 text-neutral-800"
             >
                 About the project:
             </p>
@@ -181,7 +260,7 @@ useSeoMeta({
                         delay: 350,
                     },
                 }"
-                class="font-array text-[18px] italic md:text-lg font-medium dark:text-neutral-100 text-neutral-800"
+                class="font-array text-[16px] italic font-medium dark:text-neutral-100 text-neutral-800"
             >
                 Built with:
             </p>
