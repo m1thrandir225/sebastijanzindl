1c4d7bde838a639a41a3a0cbfaec19dbbf85a9d0
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2026-01-06 22:39:32 +0100

    feat: blog-page more inclided to projects page

--- components/post-card.vue
+++ components/post-card.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts">
+import LucideIcon from '~/components/lucide-icon.vue'
+import type { Post } from '~/types/content/post'
+
+const { isMobile } = useDevice()
+
+const props = defineProps<{
+    post: Post
+    index: number
+}>()
+</script>
+
 <template>
     <NuxtLink
+        v-motion
+        :initial="{
+            opacity: 0,
+            scale: 0.95,
+            filter: 'blur(8px)',
+        }"
+        :visible-once="{
+            opacity: 1,
+            scale: 1,
+            filter: 'blur(0px)',
+            transition: {
+                delay: (() => {
+                    if (isMobile) {
+                        return index < 4 ? 100 + 75 * index : 0
+                    } else {
+                        return index < 6 ? 100 + 75 * index : 0
+                    }
+                })(),
+                duration: 300,
+            },
+        }"
         :to="'/posts/' + props.post.slug.current"
-        class="flex flex-row items-center justify-between w-full gap-4 p-6 rounded-md bg-neutral-100 shadow-lg md:shadow-none dark:bg-neutral-600 group"
+        class="flex flex-row items-center justify-between w-full gap-4 p-6 rounded-md bg-neutral-100 shadow-lg md:shadow-none dark:bg-neutral-900 hover:bg-neutral-200 dark:hover:bg-neutral-800 group"
     >
         <div class="flex flex-col items-start justify-start gap-2">
             <p
@@ -24,12 +57,3 @@
         />
     </NuxtLink>
 </template>
-
-<script setup lang="ts">
-import LucideIcon from '~/components/lucide-icon.vue'
-import type { Post } from '~/types/content/post'
-
-const props = defineProps<{
-    post: Post
-}>()
-</script>
--- pages/blog.vue
+++ pages/blog.vue
@@ -40,37 +40,73 @@
             </p>
         </div>
         <div
-            class="gap-4 flex flex-col justify-start w-full h-full py-4 px-2 md:p-8 mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl bg-neutral-100 dark:bg-neutral-900 md:bg-neutral-200 md:dark:bg-neutral-800 overflow-y-auto md:max-h-[90%] hover:scrollbar-thumb-neutral-900 active:scrollbar-thumb-neutral-900 scrollbar scrollbar-thumb-neutral-900 scrollbar-track-neutral-400/20 dark:scrollbar-thumb-neutral-100 dark:active:scrollbar-thumb-neutral-100 dark:scrollbar dark:scrollbar-track-neutral-700/20"
+            class="relative-z-50 flex flex-col max-h-full md:max-h-[600px] justify-center w-full h-full mx-auto transition-colors duration-200 ease-in-out lg:rounded-lg bg-opacity-80 dark:bg-opacity-50 backdrop-blur-3xl scrollbar-thumb-primary scrollbar-track-neutral-100 dark:scrollbar-track-neutral-900"
         >
-            <PostCard
-                v-for="(post, index) in posts"
-                :key="post.slug.current"
-                v-motion
-                :post="post"
-                :initial="{
-                    opacity: 0,
-                    scale: 0,
-                }"
-                :enter="{
-                    opacity: 100,
-                    scale: 1,
-                    transition: {
-                        delay: 400 + 75 * index,
-                    },
-                }"
-            />
+            <div v-if="status === 'pending'" class="self-center">
+                <LucideIcon
+                    name="Loader2"
+                    class="animate-spin text-neutral-900 dark:text-neutral-200"
+                    :size="64"
+                />
+            </div>
+            <div
+                v-else-if="status === 'error'"
+                class="flex flex-row items-center self-center justify-center gap-4"
+            >
+                <LucideIcon
+                    name="HeartCrack"
+                    class="text-neutral-900 dark:text-neutral-200"
+                    :size="64"
+                />
+                <p
+                    class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200"
+                >
+                    Something went wrong...
+                </p>
+            </div>
+
+            <div
+                v-else-if="status === 'success' && posts && posts.length > 0"
+                class="relative z-50 flex flex-col py-2 md:py-4 md:grid-cols-2 lg:grid-cols-1 items-start justify-start h-full gap-4 md:overflow-y-auto md:max-h-[95%] px-4 lg:px-0 transition-height ease-in-out duration-200 scrollbar-thin scrollbar-track-rounded-full sm:overflow-y-scroll scrollbar-track-neutral-200 dark:scrollbar-track-neutral-600 scrollbar-thumb-brand"
+            >
+                <div class="pr-2 w-full">
+                    <PostCard
+                        v-for="(project, index) in posts"
+                        :key="project._id"
+                        v-motion
+                        :post="project"
+                        :index="index"
+                    />
+                </div>
+            </div>
+            <div
+                v-else-if="status === 'success' && !posts"
+                class="flex items-center self-center justify-center gap-4"
+            >
+                <LucideIcon
+                    name="Meh"
+                    class="text-neutral-900 dark:text-neutral-100"
+                    :size="64"
+                />
+                <p
+                    class="font-sans text-2xl text-center text-neutral-900 dark:text-neutral-200"
+                >
+                    No projects found.
+                </p>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import PostCard from '~/components/post-card.vue'
 import type { Post } from '~/types/content/post'
 import type { BlogPageProperties } from '~/types/pages/blogPage'
 
 const query = groq`*[_type == "post"]`
 const pageQuery = groq`*[_type == "blogPage"][0]`
 
-const { data: posts } = useSanityQuery<Post[]>(query)
+const { data: posts, status: status } = useSanityQuery<Post[]>(query)
 const { data: blogPage, status: pageStatus } =
     useSanityQuery<BlogPageProperties>(pageQuery)
 
